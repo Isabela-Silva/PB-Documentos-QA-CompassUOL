3a3712db11ce5ab4a355aea9c2c1f34f191605c4
Isabela-Silva <isareginabs@gmail.com>
2025-08-24 18:11:26 -0300

    feat: add challenge calculadora

---
+++ sprint-5/Calculdadora/src/Calculadora.py
@@ -0,0 +1,18 @@
+class Calculadora:
+    def soma(self, a, b):
+        return a + b
+
+class Calculadora:
+    def soma(self, a, b):
+        return a + b
+
+    def subtracao(self, a, b):
+        return a - b
+
+    def multiplicacao(self, a, b):
+        return a * b
+
+    def divisao(self, a, b):
+        if b == 0:
+            raise ZeroDivisionError("Divisão por zero não é permitida")
+        return a / b
